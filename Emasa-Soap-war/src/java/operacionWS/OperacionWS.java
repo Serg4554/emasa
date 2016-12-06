6c2513d837f6db26423f6e647450d6e40c13dc87
Serg <Sergio.357.95@gmail.com>
2016-12-06 11:18:15 +0100

    Reconfiguración de los web services

---
+++ Emasa-Soap-war/src/java/operacionWS/OperacionWS.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionWS;
+
+import entity.ejb.Operacion;
+import entity.facade.OperacionFacade;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.jws.Oneway;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author Serg
+ */
+@WebService(serviceName = "OperacionWS")
+public class OperacionWS {
+
+    @EJB
+    private OperacionFacade ejbRef;// Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Web Service Operation")
+
+    @WebMethod(operationName = "create")
+    @Oneway
+    public void create(@WebParam(name = "entity") Operacion entity) {
+        ejbRef.create(entity);
+    }
+
+    @WebMethod(operationName = "edit")
+    @Oneway
+    public void edit(@WebParam(name = "entity") Operacion entity) {
+        ejbRef.edit(entity);
+    }
+
+    @WebMethod(operationName = "remove")
+    @Oneway
+    public void remove(@WebParam(name = "entity") Operacion entity) {
+        ejbRef.remove(entity);
+    }
+
+    @WebMethod(operationName = "find")
+    public Operacion find(@WebParam(name = "id") Object id) {
+        return ejbRef.find(id);
+    }
+
+    @WebMethod(operationName = "findAll")
+    public List<Operacion> findAll() {
+        return ejbRef.findAll();
+    }
+
+    @WebMethod(operationName = "findRange")
+    public List<Operacion> findRange(@WebParam(name = "range") int[] range) {
+        return ejbRef.findRange(range);
+    }
+
+    @WebMethod(operationName = "count")
+    public int count() {
+        return ejbRef.count();
+    }
+
+    @WebMethod(operationName = "findListaOperaciones")
+    public List<Operacion> findListaOperaciones(@WebParam(name = "id") int id) {
+        return ejbRef.findListaOperaciones(id);
+    }
+    
+}
